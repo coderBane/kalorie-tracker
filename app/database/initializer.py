bd741fb1a29309b8c5627ef8c56d3e479317cf73
coderbane <110666273+coderBane@users.noreply.github.com>
2025-05-22 18:12:38 +0100

    feat: add food models and database initialization

--- app/database/initializer.py
+++ app/database/initializer.py
@@ -1,5 +1,8 @@
+from sqlmodel import select
+
 from app.database import DatabaseContext
 from app.core.settings import app_settings
+from app.models.food import FoodCategory, FoodItem, NutritionContent
 
 
 __app_db_context = DatabaseContext(str(app_settings.DB_DSN))
@@ -18,6 +21,50 @@ def seed_db() -> None:
     """Seed the database with initial data."""
     try:
         with __app_db_context.get_session() as db_session:
-            pass
+            if db_session.exec(select(1).select_from(FoodCategory)).first() is None:
+                db_session.add_all(__food_categories)
+
+            if db_session.exec(select(1).select_from(FoodItem)).first() is None:
+                db_session.add_all(__food_items)
+            
+            db_session.commit()
     except:
         raise
+
+
+__food_categories = (
+    FoodCategory(name="Grains"),
+    FoodCategory(name="Protein"),
+    FoodCategory(name="Fruits & Vegetables"),
+    FoodCategory(name="Starchy Fruits & Tubers"),
+    FoodCategory(name="Swallow"),
+    FoodCategory(name="Soup"),
+    FoodCategory(name="Snack"),
+)
+
+__food_items = (
+    FoodItem(
+        name="Egusi Soup",
+        description="A thick, rich soup made with ground melon seeds, often served with pounded yam or fufu",
+        serving_size="1 bowl",
+        calories_per_serving=320,
+        nutrition_content=NutritionContent().model_dump(), # type: ignore
+        food_categories=[__food_categories[0], __food_categories[5]]
+    ),
+    FoodItem(
+        name="Jollof Rice",
+        description="A popular rice dish, often served with grilled meat, fish, or vegetables",
+        serving_size="100g",
+        calories_per_serving=250,
+        nutrition_content=NutritionContent().model_dump(), # type: ignore
+        food_categories=[__food_categories[0]]
+    ),
+    FoodItem(
+        name="Moin Moin",
+        description="A steamed bean pudding, often seasoned with pepper, fish, and vegetables",
+        serving_size="1 wrap",
+        calories_per_serving=400,
+        nutrition_content=NutritionContent().model_dump(), # type: ignore
+        food_categories=[__food_categories[1]]
+    )
+)
---
+++ app/models/food.py
@@ -0,0 +1,78 @@
+from uuid import UUID
+
+from sqlalchemy.dialects.postgresql import ARRAY, JSONB
+from sqlmodel import SQLModel, Column, String, Field, Relationship
+
+from app.models import AuditableEntity
+
+
+class FoodItemCategory(SQLModel, table=True):
+    """Model representing a food item category link.
+    """
+
+    __tablename__ = "food_item_category" # type: ignore
+
+    food_item_id: UUID = Field(foreign_key="food_item.id", primary_key=True)
+    food_category_id: UUID = Field(foreign_key="food_category.id", primary_key=True)
+
+
+class FoodCategory(AuditableEntity, table=True):
+    """Model representing a food category.
+    """
+
+    __tablename__ = "food_category" # type: ignore
+
+    name: str = Field(max_length=128, index=True, unique=True)
+    description: str | None = None
+    image_uri: str | None = None
+
+    food_items: list["FoodItem"] = Relationship(back_populates="food_categories", link_model=FoodItemCategory)
+
+
+class NutritionContent(SQLModel):
+    """Model representing the nutritional content of a food or meal.
+    """
+    
+    carb_g: float = Field(default=0.0, ge=0.0)
+    fat_g: float = Field(default=0.0, ge=0.0)
+    fiber_g: float = Field(default=0.0, ge=0.0)
+    protein_g: float = Field(default=0.0, ge=0.0)
+    calcium_mg: float = Field(default=0.0, ge=0.0)
+    sodium_mg: float = Field(default=0.0, ge=0.0)
+    potassium_mg: float = Field(default=0.0, ge=0.0)
+    iron_mg: float = Field(default=0.0, ge=0.0)
+    zinc_mg: float = Field(default=0.0, ge=0.0)
+
+
+class FoodItem(AuditableEntity, table=True):
+    """Model representing a food item.
+    """
+
+    __tablename__ = "food_item" # type: ignore
+
+    name: str = Field(max_length=128, index=True, unique=True)
+    description: str
+    serving_size: str = Field(max_length=32)
+    calories_per_serving: float = Field(ge=0.0)
+    nutrition_content: NutritionContent = Field(sa_column=Column(JSONB, nullable=False))
+    image_uri: str | None = None
+
+    food_categories: list[FoodCategory] = Relationship(back_populates="food_items", link_model=FoodItemCategory)
+    recipies: list["Recipe"] = Relationship(back_populates="food_item", cascade_delete=True)
+
+
+class Recipe(AuditableEntity, table=True):
+    """Model representing a recipe for a food item.
+    """
+
+    __tablename__ = "recipe" # type: ignore
+
+    food_item_id: UUID = Field(foreign_key="food_item.id", ondelete="CASCADE")
+    name: str = Field(max_length=128, index=True, unique=True)
+    description: str | None = None
+    ingredients: list[str] = Field(sa_column=Column(ARRAY(String, dimensions=1)))
+    instructions: str
+    serving_size: int = Field(gt=0)
+    nutrition_content: NutritionContent | None = Field(sa_column=Column(JSONB))
+
+    food_item: FoodItem = Relationship(back_populates="recipies")
