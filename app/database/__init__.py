6b42c574ad6c0f65ebc0e2e3eb2c9ec8fe282d42
coderbane <110666273+coderBane@users.noreply.github.com>
2025-05-26 08:22:07 +0100

    feat: add a query builder
    - implement in base repository.

--- app/database/__init__.py
+++ app/database/__init__.py
@@ -1,3 +1,4 @@
 from .base import DatabaseContext, create_db_if_not_exists
+from .query_builder import QueryBuilder
 
-__all__ = ['DatabaseContext', 'create_db_if_not_exists']
+__all__ = ['DatabaseContext', 'create_db_if_not_exists', 'QueryBuilder']
